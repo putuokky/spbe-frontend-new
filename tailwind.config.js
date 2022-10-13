dfbb5c0663937b4a3e697161d541c5c2b8a71002
Okky Maheswara <okkymahes@gmail.com>
2022-10-13 11:29:31 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["index.html"],
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14B8A6",
+        dark: "#0F172A",
+      },
+    },
+  },
+  plugins: [],
+}
